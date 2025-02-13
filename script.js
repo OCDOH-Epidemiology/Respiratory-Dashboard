02080ba112eb0681427c7920359b4454c1cfb2cf
Dcrandell-OCDOH <126595299+Dcrandell-OCDOH@users.noreply.github.com>
2025-02-13 10:20:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Declare global variables
-const updateDate = "February 10, 2025";
-const activityLevel = 1; // 1=Low; 2=Moderate; 3=High
-const activityTrend = 1; // 1=Decrease; 2=Same; 3=Increase
+const updateDate = "February 13, 2025";
+const activityLevel = 2; // 1=Low; 2=Moderate; 3=High
+const activityTrend = 2; // 1=Decrease; 2=Same; 3=Increase
 
 // On page load, update dynamic content
 document.addEventListener("DOMContentLoaded", function () {
