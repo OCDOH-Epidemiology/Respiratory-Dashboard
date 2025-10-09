27f06b88dde3750ee24e547a3e209d3a33e7fa4b
Dcrandell-OCDOH <126595299+Dcrandell-OCDOH@users.noreply.github.com>
2025-10-09 15:38:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Declare global variables
-const updateDate = "October 9, 2025";
+const updateDate = "October 10, 2025";
 const activityLevel = 1; // 1=Low; 2=Moderate; 3=High
 const activityTrend = 2; // 1=Decrease; 2=Same; 3=Increase
 
@@ -142,4 +142,5 @@ const gaugeChart = new Chart(
   document.getElementById("doughnutChart"),
   gaugeConfig
 );
+
 
