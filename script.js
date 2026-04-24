0e49a0cbc6dd8aedbc8e25c11b955bbcf77d536d
Dcrandell-OCDOH <126595299+Dcrandell-OCDOH@users.noreply.github.com>
2026-04-24 10:44:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Declare global variables
 // If you want to override the dynamic date, set updateDateOverride to a string date.
-const updateDateOverride = "April 10, 2026";
+const updateDateOverride = "April 24, 2026";
 const activityLevel = 1; // 1=Low; 2=Moderate; 3=High
 const activityTrend = 1; // 1=Decrease; 2=Same; 3=Increase
 
